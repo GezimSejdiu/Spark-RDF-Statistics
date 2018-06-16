bac6ced31a3698d09ed24162634de5de0a1648d6
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-06-16 21:29:37 +0200

    Use SANSA stats instead

--- src/main/scala/net/sansa_stack/rdf/spark/RDFStats.scala
+++ src/main/scala/net/sansa_stack/rdf/spark/RDFStats.scala
@@ -1,31 +1,74 @@
-package net.sansa.rdfstatistics.spark
+package net.sansa_stack.rdf.spark
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import scala.io.Source
-import java.io.File
-import org.apache.commons.io.FileUtils
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable.HashMap
-import org.apache.spark.graphx._
-import org.apache.spark.rdd.RDD
-import java.io.PrintWriter
-import scala.tools.nsc.io.Jar
-import scala.collection.mutable.ArrayBuffer
-import org.apache.spark.SparkConf
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION
-import net.sansa.rdfstatistics.spark.utils.Logging
-import net.sansa.rdfstatistics.spark.io._
-import net.sansa.rdfstatistics.spark.rdfstats.RDFStatistics
-import org.apache.spark.storage.StorageLevel
-import net.sansa.rdfstatistics.spark.utils.SparkUtils
 import org.apache.spark.sql.SparkSession
+import java.net.URI
+import net.sansa_stack.rdf.spark.io._
+import org.apache.jena.riot.Lang
+import scala.collection.mutable
+import java.io.File
+import net.sansa_stack.rdf.spark.stats._
 
-object App extends Logging {
+object RDFStats {
+  def main(args: Array[String]) {
+    parser.parse(args, Config()) match {
+      case Some(config) =>
+        run(config.in, config.out)
+      case None =>
+        println(parser.usage)
+    }
+  }
 
-  def main(args: Array[String]): Unit = {
+  def run(input: String, output: String): Unit = {
 
-    var sparkMasterUrl = System.getenv("SPARK_MASTER_URL")
+    val rdf_stats_file = new File(input).getName
+
+    val spark = SparkSession.builder
+      .appName(s"RDF Dataset Statistics $rdf_stats_file")
+      .master("local[*]")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate()
+
+    println("======================================")
+    println("|        RDF Statistic example       |")
+    println("======================================")
+
+    val lang = Lang.NTRIPLES
+    val startTime = System.currentTimeMillis()
+    
+    val triples = spark.rdf(lang)(input)
+
+    val stats = triples.stats
+      .voidify(rdf_stats_file, output)
+
+    println("Finished computing RDF statistics for  " + rdf_stats_file + " in " + (System.currentTimeMillis() - startTime) + "ms.")
+
+    spark.stop()
+  }
+
+  // the config object
+  case class Config(
+    in:  String = "",
+    out: String = "")
+
+  // the CLI parser
+  val parser = new scopt.OptionParser[Config]("RDF Dataset Statistics Example") {
+
+    head("RDF Dataset Statistics Example")
+
+    opt[String]('i', "input").required().valueName("<path>").
+      action((x, c) => c.copy(in = x)).
+      text("path to file that contains the data (in N-Triples format)")
+
+    opt[String]('o', "out").required().valueName("<directory>").
+      action((x, c) => c.copy(out = x)).
+      text("the output directory")
+
+    help("help").text("prints this usage text")
+  }
+}
+
+  /*def main(args: Array[String]): Unit = {
+
     val file = args(0)
     val rdf_stats_file = new File(file).getName
     val outputPath =args(1)
@@ -57,3 +100,4 @@ object App extends Logging {
   }
 }
 
+*/
